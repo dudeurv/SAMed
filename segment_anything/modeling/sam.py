54d87bd812b7054a41bd8d91d8f1886dd3e16950
dudeurv <144495312+dudeurv@users.noreply.github.com>
2023-12-21 00:11:43 +0000

    Update sam.py

--- segment_anything/modeling/sam.py
+++ segment_anything/modeling/sam.py
@@ -25,8 +25,8 @@ class Sam(nn.Module):
         image_encoder: ImageEncoderViT,
         prompt_encoder: PromptEncoder,
         mask_decoder: MaskDecoder,
-        pixel_mean: List[float] = [123.675, 116.28, 103.53],
-        pixel_std: List[float] = [58.395, 57.12, 57.375],
+        pixel_mean: List[float] = [123.675],  # Adjusted for grayscale
+        pixel_std: List[float] = [58.395],    # Adjusted for grayscale
     ) -> None:
         """
         SAM predicts object masks from an image and input prompts.
