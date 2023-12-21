74c296a086fd6850167b4d0df9625c8c4e34a3b6
dudeurv <144495312+dudeurv@users.noreply.github.com>
2023-12-21 01:08:27 +0000

    Update trainer_BraTS.py

--- trainer_BraTS.py
+++ trainer_BraTS.py
@@ -71,7 +71,7 @@ def trainer_BraTS(args, model, snapshot_path, multimask_output, low_res):
     skip_hard_nums = 0
     for epoch_num in iterator:
         for i_batch, (image_batch, label_batch) in enumerate(trainloader):
-            image_batch, label_batch = image_batch.cuda(), label_batch.cuda()
+            image_batch, label_batch = image_batch.unsqueeze(1).cuda(), label_batch.unsqueeze(1).cuda()
             assert image_batch.max() <= 3, f'image_batch max: {image_batch.max()}'
             if args.use_amp:
                 with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=args.use_amp):
