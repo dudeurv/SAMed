111e24b9f2f97348a20045419e5128ec20c6509b
dudeurv <144495312+dudeurv@users.noreply.github.com>
2024-01-05 13:34:40 +0000

    Update run_all_V2.sh

--- Cluster/run_all_V2.sh
+++ Cluster/run_all_V2.sh
@@ -1,10 +1,26 @@
+#!/bin/bash -l
+#$ -l gpu=1
+#$ -l h_rt=0:10:0
+#$ -l tmem=1G
+#$ -N myJobName
+#$ -wd /home/ududeja
+
+# Load the Python module
+module load python/3.8
+
+# Activate the virtual environment
+source /home/ududeja/LapVideoUS-SUSI/bin/activate
+
+# Navigate to the directory containing the scripts
+cd /home/ududeja
+
 # Run the setup script
-echo "Running setup..."
+echo "Running SAMed_BraTS_setup.py..."
 python SAMed_BraTS_setup.py
 
 # Check if setup was successful, then run the data loader script
 if [ $? -eq 0 ]; then
-    echo "Setup completed successfully. Proceeding to data loading..."
+    echo "Setup completed successfully. Proceeding to SAMed_BraTS_dataloader.py..."
     python SAMed_BraTS_dataloader.py
 else
     echo "Setup failed. Exiting..."
@@ -13,7 +29,7 @@ fi
 
 # Check if data loading was successful, then run the training script
 if [ $? -eq 0 ]; then
-    echo "Data loading completed successfully. Proceeding to training..."
+    echo "Data loading completed successfully. Proceeding to SAMed_BraTS_V2_training.py..."
     python SAMed_BraTS_V2_training.py
 else
     echo "Data loading failed. Exiting..."
@@ -22,7 +38,7 @@ fi
 
 # Check if training was successful, then run the inference script
 if [ $? -eq 0 ]; then
-    echo "Training completed successfully. Proceeding to inference..."
+    echo "Training completed successfully. Proceeding to SAMed_BraTS_inference.py..."
     python SAMed_BraTS_inference.py
 else
     echo "Training failed. Exiting..."
