e196f923346460fff3b77d6420330319cb2e0dce
dudeurv <144495312+dudeurv@users.noreply.github.com>
2024-01-05 16:11:34 +0000

    Create SAMed_BraTS_V4_5_training.py

---
+++ Cluster/SAMed_BraTS_V4_5_training.py
@@ -0,0 +1,173 @@
+import os
+
+# Change directory to 'desired/path'
+os.chdir('/content/samed_codes')
+
+import sys
+from tqdm import tqdm
+import logging
+import numpy as np
+import argparse
+import random
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+import torch.backends.cudnn as cudnn
+from importlib import import_module
+from segment_anything import sam_model_registry
+from datasets.dataset_synapse import Synapse_dataset
+from icecream import ic
+from medpy import metric
+from scipy.ndimage import zoom
+import torch.nn as nn
+import SimpleITK as sitk
+import torch.nn.functional as F
+import imageio
+from einops import repeat
+
+from torch.nn.modules.loss import CrossEntropyLoss
+from utils import DiceLoss
+import torch.optim as optim
+# from dataset_brats import BratsDataset
+
+
+def seed_everything(seed=42):
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+def calc_loss(outputs, low_res_label_batch, ce_loss, dice_loss, dice_weight:float=0.8):
+    low_res_logits = outputs['low_res_logits']
+    loss_ce = ce_loss(low_res_logits, low_res_label_batch[:].long())
+    loss_dice = dice_loss(low_res_logits, low_res_label_batch, softmax=True)
+    loss = (1 - dice_weight) * loss_ce + dice_weight * loss_dice
+    return loss, loss_ce, loss_dice
+
+def training_per_epoch(model, trainloader, optimizer, iter_num, ce_loss, dice_loss, multimask_output=True, args=None):
+    model.train()
+    loss_all = []
+    for i_batch, sampled_batch in enumerate(trainloader):
+        image_batch, label_batch, low_res_label_batch = sampled_batch['image'],sampled_batch['label'], sampled_batch['low_res_label']
+        image_batch, label_batch, low_res_label_batch = image_batch.to(device, dtype=torch.float32), label_batch.to(device, dtype=torch.long), low_res_label_batch.to(device, dtype=torch.long)
+        outputs = model(image_batch, multimask_output, args.img_size)
+        loss, loss_ce, loss_dice = calc_loss(outputs, low_res_label_batch, ce_loss, dice_loss)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        loss_all.append(loss.item())
+        if args.warmup and iter_num < args.warmup_period:
+            lr_ = args.base_lr * ((iter_num + 1) / args.warmup_period)
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr_
+        else:
+            if args.warmup:
+                shift_iter = iter_num - args.warmup_period
+                assert shift_iter >= 0, f'Shift iter is {shift_iter}, smaller than zero'
+            else:
+                shift_iter = iter_num
+            lr_ = args.base_lr * (1.0 - shift_iter / args.max_iterations) ** 0.9  # learning rate adjustment depends on the max iterations
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr_
+
+        iter_num = iter_num + 1
+
+    return np.mean(loss_all)
+
+
+def test_per_epoch(model, testloader, ce_loss, dice_loss, multimask_output=True, args=None):
+    model.eval()
+    loss_per_epoch, dice_per_epoch = [], []
+    with torch.no_grad():
+        for i_batch, sampled_batch in enumerate(testloader):
+            image_batch, label_batch, low_res_label_batch = sampled_batch['image'],sampled_batch['label'], sampled_batch['low_res_label']
+            image_batch, label_batch, low_res_label_batch = image_batch.to(device, dtype=torch.float32), label_batch.to(device, dtype=torch.long), low_res_label_batch.to(device, dtype=torch.long)
+            outputs = model(image_batch, multimask_output, args.img_size)
+            loss, loss_ce, loss_dice = calc_loss(outputs, low_res_label_batch, ce_loss, dice_loss)
+            loss_per_epoch.append(loss.item())
+            dice_per_epoch.append(1-loss_dice.item())
+    return np.mean(loss_per_epoch), np.mean(dice_per_epoch)
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default=None, help='The config file provided by the trained model')
+    parser.add_argument('--volume_path', type=str, default='testset/test_vol_h5/')
+    parser.add_argument('--dataset', type=str, default='Synapse', help='Experiment name')
+    parser.add_argument('--num_classes', type=int, default=3)
+    parser.add_argument('--list_dir', type=str, default='./lists/lists_Synapse/', help='list_dir')
+    parser.add_argument('--output_dir', type=str, default='results')
+    parser.add_argument('--img_size', type=int, default=512, help='Input image size of the network')
+    parser.add_argument('--input_size', type=int, default=224, help='The input size for training SAM model')
+    parser.add_argument('--seed', type=int,
+                        default=1234, help='random seed')
+    parser.add_argument('--is_savenii', action='store_true', help='Whether to save results during inference')
+    parser.add_argument('--deterministic', type=int, default=1, help='whether use deterministic training')
+    parser.add_argument('--ckpt', type=str, default='checkpoints/sam_vit_b_01ec64.pth',
+                        help='Pretrained checkpoint')
+    parser.add_argument('--lora_ckpt', type=str, default='checkpoints/epoch_159.pth', help='The checkpoint from LoRA')
+    parser.add_argument('--vit_name', type=str, default='vit_b', help='Select one vit model')
+    parser.add_argument('--rank', type=int, default=7, help='Rank for LoRA adaptation')
+    parser.add_argument('--module', type=str, default='sam_lora_image_encoder')
+
+    parser.add_argument('--base_lr', type=float, default=0.005, help='segmentation network learning rate')
+    parser.add_argument('--batch_size', type=int, default=12, help='batch_size per gpu')
+    parser.add_argument('--warmup', type=bool, default=True, help='If activated, warp up the learning from a lower lr to the base_lr')
+    parser.add_argument('--warmup_period', type=int, default=250, help='Warp up iterations, only valid whrn warmup is activated')
+    parser.add_argument('--AdamW', type=bool, default=True, help='If activated, use AdamW to finetune SAM model')
+    parser.add_argument('--max_epochs', type=int, default=80, help='maximum epoch number to train')
+    parser.add_argument('--max_iterations', type=int, default=30000, help='maximum epoch number to train')
+
+
+    if 'ipykernel' in sys.modules:
+        args = parser.parse_args([])
+    else:
+        args = parser.parse_args()
+
+    args.output_dir = 'results'
+    args.ckpt = 'sam_vit_b_01ec64.pth'
+    args.lora_ckpt = 'epoch_159.pth'
+    os.makedirs(args.output_dir, exist_ok = True)
+
+    sam, img_embedding_size = sam_model_registry[args.vit_name](image_size=args.img_size,
+                                                                    num_classes=args.num_classes,
+                                                                    checkpoint=args.ckpt, pixel_mean=[0, 0, 0],
+                                                                    pixel_std=[1, 1, 1])
+
+    pkg = import_module(args.module)
+    net = pkg.LoRA_Sam(sam, args.rank).cuda()
+    # net.load_lora_parameters(args.lora_ckpt)
+    multimask_output = True if args.num_classes > 1 else False
+    train_dataset = BratsDataset(root='Slices/Train', low_res=128, isTrain=True)
+    test_dataset = BratsDataset(root='Slices/Test', low_res=128)
+    trainloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    testloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+    print('Training on:', device, 'train sample size:', len(train_dataset), 'test sample size:', len(test_dataset), 'batch:', args.batch_size)
+
+    ce_loss = CrossEntropyLoss()
+    dice_loss = DiceLoss(args.num_classes + 1)
+    b_lr = args.base_lr / args.warmup_period
+    optimizer = optim.AdamW(filter(lambda p: p.requires_grad, net.parameters()), lr=b_lr, betas=(0.9, 0.999), weight_decay=0.1)
+    iter_num = 0
+
+    best_epoch, best_loss = 0.0, np.inf
+    for epoch in range(args.max_epochs):
+        loss_training = training_per_epoch(net, trainloader, optimizer, iter_num, ce_loss, dice_loss, multimask_output=multimask_output, args=args)
+        loss_testing, dice = test_per_epoch(net, testloader, ce_loss, dice_loss,multimask_output=True, args=args)
+
+        if loss_testing < best_loss:
+            best_loss = loss_testing
+            best_epoch = epoch
+            net.save_lora_parameters(os.path.join(args.output_dir, 'model_best.pt'))
+
+        print('--- Epoch {}\{}: Training loss = {:.4f}, Testing: [loss = {:.4f}, dice = {:.4f}], Best loss = {:.4f}, Best epoch = {}, lr = {:.6f}'.\
+            format(epoch, args.max_epochs, loss_training, loss_testing, dice, best_loss, best_epoch, optimizer.param_groups[0]['lr']))
+
+
+if __name__ == '__main__':
+    seed_everything()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    main()
