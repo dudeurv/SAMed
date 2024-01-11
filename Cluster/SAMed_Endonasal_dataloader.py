6095cee900e774c07102d5c3d81a2203632b3aa0
dudeurv <144495312+dudeurv@users.noreply.github.com>
2024-01-11 14:29:09 +0000

    Update SAMed_Endonasal_dataloader.py

--- Cluster/SAMed_Endonasal_dataloader.py
+++ Cluster/SAMed_Endonasal_dataloader.py
@@ -1,1 +1,76 @@
+from torch.utils.data import Dataset, DataLoader
+from glob import glob
+import imageio as iio
+import matplotlib.pyplot as plt
+from glob import glob
+import numpy as np
+from scipy.ndimage import zoom
+from einops import repeat
+from scipy import ndimage
+import random
+from PIL import Image
+import cv2
+
+def normalise_intensity(image, ROI_thres=0.1):
+    pixel_thres = np.percentile(image, ROI_thres)
+    ROI = np.where(image > pixel_thres, image, 0) # If image value is greater than pixel threshold, return image value, otherwise return 0
+    mean = np.mean(ROI)
+    std = np.std(ROI)
+    ROI_norm = (ROI - mean) / (std + 1e-8) # Normalise ROI
+    return ROI_norm
+
+def random_rot_flip(image, label):
+    k = np.random.randint(0, 4)
+    image = np.rot90(image, k)
+    label = np.rot90(label, k)
+    axis = np.random.randint(0, 2)
+    image = np.flip(image, axis=axis).copy()
+    label = np.flip(label, axis=axis).copy()
+    return image, label
+
+
+def random_rotate(image, label):
+    angle = np.random.randint(-20, 20)
+    image = ndimage.rotate(image, angle, order=0, reshape=False)
+    label = ndimage.rotate(label, angle, order=0, reshape=False)
+    return image, label
+
+def map_labels(label):
+    label_map = {0: 0, 85: 1, 128:0, 170: 2, 255: 3}
+    mapped_label = label.copy()
+    for k, v in label_map.items():
+        mapped_label[label == k] = v
+    return mapped_label
+
+class EndonasalDataset(Dataset):
+    def __init__(self, root='endonasal_train', low_res=None, isTrain=False):
+        self.img_path_all = glob(root + '/mri_t1c/*.png')  # Update the path and pattern
+        self.mask_path_all = glob(root + '/mri_masks/*.png')  # Update the path and pattern
+        self.isTrain = isTrain
+        self.isTrain = isTrain
+        self.low_res = low_res
+
+    def __len__(self):
+      return len(self.img_path_all)
+
+    def __getitem__(self, index):
+        image = iio.imread(self.img_path_all[index])
+        image = normalise_intensity(image)
+        image = zoom(image, (512/image.shape[0], 512/image.shape[1]), order=0)
+        label = iio.imread(self.mask_path_all[index])
+        label = map_labels(label)
+        label = zoom(label, (512/label.shape[0], 512/label.shape[1]), order=0)
+        if self.isTrain:
+            if random.random() > 0.5:
+                image, label = random_rot_flip(image, label)
+            elif random.random() > 0.5:
+                image, label = random_rotate(image, label)
+
+        image = repeat(np.expand_dims(image, axis=0), 'c h w -> (repeat c) h w', repeat=3)
+        sample = {'image': image, 'label': label}
+        if self.low_res:
+            low_res_label = zoom(label, (self.low_res/label.shape[0], self.low_res/label.shape[1]), order=0)
+            sample = {'image': image, 'label': label, 'low_res_label': low_res_label}
+
+        return sample
 
